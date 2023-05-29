eae22963d7db5890fe91adb1351d5d31dba967f2
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-05-29 13:55:31 +0700

    membuat repository main.go yg import ke sayhalo dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/irfanyazid-13032000/app-say-halo
+
+go 1.20
+
+require github.com/irfanyazid-13032000/go-say-halo v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_halo "github.com/irfanyazid-13032000/go-say-halo"
+)
+
+func main() {
+	fmt.Println(go_say_halo.SayHalo())
+}
